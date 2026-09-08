1a5b03f28de281c1fbfae94d1415363ea0dd8dbf
Đình Viễn <nguyendinhvien2004@gmail.com>
2026-09-08 16:54:18 +0700

    Disable SMTP debug output and use configured sender

--- assets/php/send_code.php
+++ assets/php/send_code.php
@@ -1,44 +1,45 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 $smtpConfig = require 'smtp_config.php';
-
-//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-function sendCode($email,$subject,$code){
-global $mail, $smtpConfig;
+function sendCode($email, $subject, $code)
+{
+    global $mail, $smtpConfig;
+
     try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = $smtpConfig['username'];                     //SMTP username
-        $mail->Password   = $smtpConfig['password'];                     //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-    
-        //Recipients
-        $mail->setFrom('vanthangnguyen269@gmail.com', 'Handbook');    //Add a recipient
-        $mail->addAddress($email);               //Name is optional
-    
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $smtpConfig['username'];
+        $mail->Password = $smtpConfig['password'];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+        $mail->CharSet = 'UTF-8';
+
+        $mail->clearAllRecipients();
+        $mail->clearAttachments();
+        $mail->setFrom($smtpConfig['username'], 'Handbook');
+        $mail->addAddress($email);
+
+        $mail->isHTML(true);
         $mail->Subject = $subject;
         $template = file_get_contents(__DIR__ . '/email_template.html');
-        $template = str_replace('{{CODE}}', $code, $template);
+        $template = str_replace('{{CODE}}', htmlspecialchars((string) $code, ENT_QUOTES, 'UTF-8'), $template);
         $mail->Body = $template;
         $mail->addEmbeddedImage(__DIR__ . '/../images/icon.png', 'logo');
         $mail->send();
+        return true;
     } catch (Exception $e) {
-        echo "Không thể gửi tin nhắn. Lỗi Mailer:{$mail->ErrorInfo}";
+        error_log('Handbook mail error: ' . $mail->ErrorInfo);
+        return false;
     }
-    
 }
+?>
