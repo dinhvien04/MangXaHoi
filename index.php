9ad5755492bfb91d35b43457f5e6d1d3a7babe33
Đình Viễn <nguyendinhvien2004@gmail.com>
2026-09-09 23:00:37 +0700

    refactor: use unified front controller for admin

--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 
 require_once __DIR__ . '/backend/bootstrap.php';
 
+if (isset($_GET['admin'])) {
+    if (isset($_GET['action'])) {
+        require __DIR__ . '/backend/http/admin-actions.php';
+        exit();
+    }
+
+    if (isset($_GET['api'])) {
+        require __DIR__ . '/backend/http/admin-api.php';
+        exit();
+    }
+
+    require __DIR__ . '/frontend/admin/router.php';
+    exit();
+}
+
 if (isset($_GET['action'])) {
     require __DIR__ . '/backend/http/user-actions.php';
     exit();
